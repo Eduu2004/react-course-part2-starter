eb1d5a2f12e5149e8bf66f1aafb29af53fd2d94a
Eduu2004 <a20edurenlop@inspedralbes.cat>
2025-10-28 09:39:23 +0100

    reusable api

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,54 +1,12 @@
-import {
-  QueryClient,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
 import { useRef } from "react";
-import { Todo } from "./hooks/useTodos";
-import axios from "axios";
-
-//Acabar de mirar ultim miut de 18- Optimistic Update!!
-
-interface AddTodoContext {
-  previousTodos: Todo[]
-
-}
+import useAddTodo from "./hooks/useAddTodo";
 
 const TodoForm = () => {
-  const queryClient = useQueryClient();
-
-  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-
-    onMutate: (newTodo: Todo) => {
-      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
-
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        newTodo,
-        ...(todos || []),
-      ]);
-
-      if (ref.current) ref.current.value = "";
-
-      return { previousTodos };
-    },
-    onSuccess: (savedTodo, newTodo) => {
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
-        todos?.map((todo) =>
-          todo === newTodo ? savedTodo : todo)
-      );
-    },
-
-    onError: (error, newTodo, context) => {
-      if (!context) return;
-
-      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos)
-    }
-  });
+  
   const ref = useRef<HTMLInputElement>(null);
+  const addTodo = useAddTodo(() => {
+      if (ref.current) ref.current.value = "";
+  })
 
   return (
     <>
@@ -73,8 +31,8 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button disabled={addTodo.isLoading} className="btn btn-primary">
-            {addTodo.isLoading ? "Adding..." : "Add"}
+          <button className="btn btn-primary">
+            Add
           </button>
         </div>
       </form>
