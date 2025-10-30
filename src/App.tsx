6a1e881f63912f30bdb5c14dba995501deae763a
Eduu2004 <a20edurenlop@inspedralbes.cat>
2025-10-30 10:54:28 +0100

    implementar zustand a l'aplicació

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./state-management/NavBar";
 import { TaskList, TasksProviders } from "./state-management/tasks";
 import AuthProvider from "./state-management/auth/AuthProvider";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <>
       <AuthProvider>
         <TasksProviders>
+          <Counter />
           <NavBar />
           <TaskList />
         </TasksProviders>
